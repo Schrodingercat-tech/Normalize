3a7bb25655726a30fcd9fee0052c73c973435b59
Sai Ganesh Reddy <ganesh.sai432@gmail.com>
2024-01-17 10:59:27 +0530

    front end cropping image in react starting

---
+++ frontend/src/sai-test-assects/home.jsx
@@ -0,0 +1,13 @@
+const img = '/public/images/girl.jpg'
+
+
+
+export function Test(){
+    return(
+        <div>
+            <p> this task is to crop image with in react</p>
+            <img src={img} alt="description" />
+        </div>
+    );
+}
+
