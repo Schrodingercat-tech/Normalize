041ae0fc1661e7ea0354638e4c0ec487f1f0e705
Sai Ganesh Reddy <ganesh.sai432@gmail.com>
2024-01-17 18:03:50 +0530

    pkgs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 // import {CroppedImage} from "./sai-test-assects/home";
 // const img = '/public/images/girl.jpg'
-
-import { Wish } from "./sai-test-assects/home";
+import { Example } from "./sai-test-assects/assects/tailwind components/page";
 
 function App() {
   return (
     <div className="cropped-container">
-    <h1 className='text-3xl font-bold underline text-center'>Normalize</h1>
-      <Wish />
+    <Example />
     </div>
   );
 }
