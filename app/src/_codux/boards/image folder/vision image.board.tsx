db7e36d48a2420fee9108c434aafa47cef6de658
Sai Ganesh Reddy <ganesh.sai432@gmail.com>
2024-01-23 15:31:12 +0530

    card

---
+++ app/src/_codux/boards/image folder/vision image.board.tsx
@@ -0,0 +1,14 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'Button 1',
+    Board: () =><div>
+<img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" alt="" />
+<img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" alt="" />
+<div></div>
+<div></div></div>,
+    isSnippet: true,
+    environmentProps: {
+canvasWidth: 804,
+canvasHeight: 273,}
+});
---
+++ app/src/page/image card.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+
+export function ImageCard(){
+
+    return(
+        <>
+            <Card>
+
+            </Card>
+        </>
+    );
+}
